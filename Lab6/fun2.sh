90ebb63f8eb043e40bfd466a1ff847b2bc9368dc
Jacek Papis <jacek@MacBook-Pro-Jacek.local>
2016-11-28 20:11:02 +0100

    made fun2.sh and wywolaj2.sh functional

--- Lab6/fun2.sh
+++ Lab6/fun2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 function policz ()
 {
-if[$zmienna == 0] ; then
-echo 1
-else
-echo $[$zmienna * `policz $[$zmienna - 1]`]
-fi
-
+b=$[RANDOM%(11-5)+5]
+echo "Wylosowana liczba:"
+echo $b
+wynik=1
+for (( i=1; $b+1 > i; i++)) ; do
+wynik=$[wynik*i]
+done
+echo "Silnia:"
+echo $wynik
 }
--- Lab6/wywolaj2.sh
+++ Lab6/wywolaj2.sh
@@ -3,7 +3,5 @@
 a=$[RANDOM%20]
 for (( j=0; $j < a; j++ )) ; do
 . fun2.sh
-b=$[RANDOM%(10-5)+5]
-policz(b)
+policz
 done
-
