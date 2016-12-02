feb88df2aeea5641c9588f917a23618e8d62a71a
darktX <DarktX@users.noreply.github.com>
2016-12-02 16:31:55 +0100

    added files

--- Lab6/arg3.sh
+++ Lab6/arg3.sh
@@ -1,40 +1,15 @@
 #!/bin/bash
-a=$1
-b=$2
-c=$3
-zmienna=0
-
-if [ $1 == "" ]; then
+i=3
+if [[ $1 == "" ]] && [[ $2 == "" ]] && [[ $3 == "" ]] ; then  # wpisano 0 argumentow
 exit 1
-#fi
-elif [ $2 == "" ]; then
-exit 1
-#zmienna=zmienna+1
-#fi
-elif [ $3 == "" ]; then
-zmienna=2
-#fi
-else 
-#[ $3 == "" ]; then
-zmienna=3
-fi
-
- 
-if [ $zmienna -eq 2 ]; then
-echo $a
-echo $b
-echo $c
-fi
-if [ $zmienna -eq 3 ]; then
-
-
-x=3
-while [ $x -lt 0 ] ; do
-echo $a
-echo $b
-echo $c
-x=$[ x-1 ]
+elif [[ $2 == "" ]] && [[ $3 == "" ]] ; then # wpisano 1 argument
+echo $1
+elif [[ $3 == "" ]] ; then  # wpisano 2 argumenty
+echo $1 $2
+else  # wpisano 3 argumenty
+while [ $i -gt 0 ] ; do
+echo $1 $2 $3
+i=$[i-1]
 done
 fi
-
 
--- Lab6/odlicz.sh
+++ Lab6/odlicz.sh
@@ -11,3 +11,26 @@ echo "Pozostalo $b sekund"
 sleep 1
 b=$[b-1]
 done
+
+
+
+
+
+
+
+
+
+#find ~/ -perm 700 >file.dat
+#grep '.$' file.dat >ukryte.dat
+
+
+
+
+
+#if [ $a -lt $b ]; then
+#echo $[a+b]
+#elif [ $a -gt $b ]; then
+#echo $[a-b]
+#else
+#echo $[a*b]
+#fi
